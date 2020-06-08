cc0de2916509c8a31aeee86ac624e25f49534fa8
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-08 15:39:34 -0300

    revisar actaDeKeene

--- src/Lib.hs
+++ src/Lib.hs
@@ -19,18 +19,19 @@ agentesDelGobierno = [("Jack Bauer","24"),
 
 
 --------Eventos------------
-type Vigilante = (String,[String],Int)--como es de mas de dos elementos no puedo hacer fst y snd
+type Habilidades = [String]
+type Vigilante = (String,Habilidades,Int)--como es de mas de dos elementos no puedo hacer fst y snd
 type Agente = (String,String)
 type Evento = [Vigilante]->[Vigilante]
 
 tomoElPrimero :: Vigilante->String
-tomoElPrimero (nombre,_,_) = nombre
+tomoElPrimero (nombre,_,_) = nombre--como es de mas de dos elementos tengo que acceder a los datos asi
 
 destruccionDeNiuShork :: Evento
 destruccionDeNiuShork = filter (not.sacoVigilante "Rorschach").filter (not.sacoVigilante "Dr.manhattan")
 
 sacoVigilante :: String->Vigilante->Bool
-sacoVigilante vigilanteASacar (nombre,_,_) =  nombre == vigilanteASacar
+sacoVigilante vigilanteASacar (nombre,_,_) = (== vigilanteASacar) nombre
 
 muerteDeUnVigilante :: String->Evento
 muerteDeUnVigilante nombre = filter (sacoVigilante nombre)
@@ -44,6 +45,25 @@ agregarHabilidad habilidad (nombre, habilidades, anio) = (nombre,habilidad:habil
  
 esAgenteDelGobierno :: [Vigilante]->[Vigilante]
 esAgenteDelGobierno vigilantes = filter (estaEn agentesDelGobierno) vigilantes
+--le paso la lista con los nombres de los agentes del gob. y me fijo si el vigilante que agarra el filter esta en esa lista.
+--en caso de que este lo guarda en la lista nueva con solo los agentes del gob y les agrego a todos cinismo en habilidades.
 
 estaEn :: [Agente]->Vigilante->Bool
 estaEn agentes (nombre,_,_)= any (==nombre) (map fst agentes)
+
+accidenteDeLab :: Int->[Vigilante]
+accidenteDeLab anio = ("Dr.Manhattam",["manipulacion de materia a nivel atomico"],anio):algunosVigilantes
+--de vuelta uso : y no ++ pq solo le agrego un elemento y no una lista de elementos
+
+actaDeKeene :: [Vigilante]->[Vigilante]
+actaDeKeene listaEntera |any (comparoNombres listaEntera) algunosVigilantes = sacoVigilanteAnterior listaEntera (head listaEntera)
+                        |otherwise = listaEntera -- si no hay sucesor devielve la lista como esta
+
+comparoNombres :: [Vigilante]->Vigilante->Bool
+comparoNombres lista (nombre,_,_) = ((==nombre).tomoElPrimero.head) lista
+
+sacoVigilanteAnterior :: [Vigilante]->Vigilante->[Vigilante]
+sacoVigilanteAnterior lista vigilante = dropWhile (esSucesor vigilante) lista 
+
+esSucesor:: Vigilante->Vigilante->Bool
+esSucesor (nombre,_,anio) (otroNombre,_,otroAnio) = (nombre == otroNombre) && (anio > otroAnio)
